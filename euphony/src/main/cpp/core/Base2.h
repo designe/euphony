bbda6e342bf075d434ca80a9843dda0b5f6d9533
designe <hello.designe@facebook.com>
2021-09-04 23:19:56 +0900

    Added Base2 Feature [Unit-test Success]

--- euphony/src/main/cpp/core/Base2.h
+++ euphony/src/main/cpp/core/Base2.h
@@ -1,15 +1,23 @@
 #ifndef EUPHONY_BASE2_H
 #define EUPHONY_BASE2_H
 
+#include "Base.h"
+
 namespace Euphony {
 
-    class Base2 : public BaseCodec {
+    class Base2 : public Base {
     public:
-        Base2() = default;
+        Base2(const HexVector &hexVectorSrc);
         ~Base2() = default;
-        string encode(string src);
-        string decode(string src);
-        int convertChar2Int(char source);
+        std::string getBaseString();
+        const HexVector &getHexVector() const;
+        int convertChar2Int(char source) const;
+        char convertInt2Char(int source) const;
+
+    private:
+        HexVector hexVector;
+
+        std::string hexToBase2(u_int8_t hex);
     };
 }
 
